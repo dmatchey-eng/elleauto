27d7636903592f5979538b12a07e370d8a5b82fd
dmatchey-eng <dmatchey@gmail.com>
2026-06-16 10:05:14 -0500

    Add miner_types.h with HostUlong4 and StratumJob structs

---
+++ include/miner_types.h
@@ -0,0 +1,21 @@
+#ifndef MINER_TYPES_H
+#define MINER_TYPES_H
+
+#include <string>
+
+// 🚀 SHARED STRUCT: Now safely visible to all translation units!
+struct alignas(32) HostUlong4 {
+    unsigned long long s0 = 0;
+    unsigned long long s1 = 0;
+    unsigned long long s2 = 0;
+    unsigned long long s3 = 0;
+};
+
+struct StratumJob {
+    std::string job_id = "";
+    std::string block_height_hex = "";  
+    std::string header_hash_hex = ""; 
+    bool is_new_job = false;
+};
+
+#endif // MINER_TYPES_H
